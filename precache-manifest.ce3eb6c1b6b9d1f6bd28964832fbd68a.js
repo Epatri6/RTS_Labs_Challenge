a6e9d007e9d222d86448eef9637c247b035c1bcf
Epatri6 <65800055+Epatri6@users.noreply.github.com>
2020-10-18 05:38:22 -0400

    Updates

---
+++ precache-manifest.ce3eb6c1b6b9d1f6bd28964832fbd68a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a6dad5ba1d61bec27c89ed57e6af074",
+    "url": "/RTS_Labs_Challenge/index.html"
+  },
+  {
+    "revision": "6388e2456f566493a7db",
+    "url": "/RTS_Labs_Challenge/static/css/main.8330e30c.chunk.css"
+  },
+  {
+    "revision": "e5213363b05f24cce8b2",
+    "url": "/RTS_Labs_Challenge/static/js/2.682b70f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RTS_Labs_Challenge/static/js/2.682b70f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6388e2456f566493a7db",
+    "url": "/RTS_Labs_Challenge/static/js/main.7e4ee070.chunk.js"
+  },
+  {
+    "revision": "07df2065ea8d532f8e73",
+    "url": "/RTS_Labs_Challenge/static/js/runtime-main.52cc71de.js"
+  }
+]);
